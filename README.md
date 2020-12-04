60cca0c5ebe471a9af16e0ff78c219e38ef3e998
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2020-12-04 13:06:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flare_app
+ Projeto de construção de um App utilizando o flare
---
+++ android/app/src/main/java/br/com/ctisistemas/flare_app/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.ctisistemas.flare_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
